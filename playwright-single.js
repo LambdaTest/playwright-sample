7b9a91c988a7114de0e2a706a3704c82beda2051
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2021-11-22 20:20:32 +0530

    Create playwright-single.js

---
+++ playwright-single.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect
+const { chromium } = require('playwright');
+
+(async () => {
+  const capabilities = {
+    'browserName': 'chrome',
+    'version': 'latest',
+    'platform': 'MacOS Catalina',
+    'build': 'Playwright Sample Build',
+    'name': 'Playwright Sample Test',
+    'username': '<your user name>',
+    'access_key': '<your access key>',
+  };
+
+  const browser = await chromium.connectOverCDP({
+    endpointURL: `wss://stage-cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://www.bing.com');
+
+  const element = await page.$('[aria-label="Enter your search term"]');
+  await element.click();
+  await element.type('LambdaTest');
+  await element.press('Enter');
+  const title = await page.title();
+
+  expect(title).to.equal("LambdaTest - Search", 'Incorrect title!');
+
+  await browser.close();
+})();

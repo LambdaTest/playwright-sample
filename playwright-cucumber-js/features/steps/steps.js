97ca1fe1406350e3369ad9162eabcc2e0c06d4e5
Ankur Narkhede <ankurn@lambdatest.com>
2022-04-04 14:27:05 +0530

    Add support for running tests on playwright cucumber-js

---
+++ playwright-cucumber-js/features/steps/steps.js
@@ -0,0 +1,25 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+const assert = require("assert");
+
+Given("Open LambdaTest Website", { timeout: 60 * 1000 }, async function () {
+  await this.openUrl('https://www.lambdatest.com/');
+});
+
+When("Open HyperExecute page", async function () {
+  await this.page.hover('text=Platform')
+  await this.page.click('h3:has-text("HyperExecute")');
+});
+
+Then("Open HyperExecute documentation", async function () {
+  await this.page.click('text=Read More');
+  let title = await this.page.title()
+
+  try{
+    assert.equal(title, "How1 to use HyperExecute for scalable and reliable web automation testing | LambdaTest", "Page title does not match")
+
+    await this.setTestStatus("passed", "Title matched")
+  } catch (e) {
+    await this.setTestStatus("failed", e)
+    throw(e)
+  }
+});
---
+++ playwright-cucumber-js/features/view-hyperexecute-documentation.feature
@@ -0,0 +1,6 @@
+Feature: View HyperExecute Documentation
+
+  Scenario: Go to HyperExecute Documentation from LambdaTest Website
+    Given Open LambdaTest Website
+    When Open HyperExecute page
+    Then Open HyperExecute documentation

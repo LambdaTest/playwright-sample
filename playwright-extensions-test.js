d10faf8a4dd72af829af8c475bc5488a2552b390
Ankur Narkhede <84451419+ankur-lt@users.noreply.github.com>
2022-09-27 14:35:52 +0530

    Create playwright-extensions-test.js

---
+++ playwright-extensions-test.js
@@ -0,0 +1,61 @@
+const {chromium} = require("playwright");
+const {expect} = require("@playwright/test");
+
+(async () => {
+  let browser;
+  try {
+    const capabilities = {
+      "browserName": "Chrome",
+      "browserVersion": "latest",
+      "LT:Options": {
+        "platform": "MacOS Big sur",
+        "build": "Playwright Extensions test",
+        "name": "Load extension and verify it",
+        "user": process.env.LT_USERNAME,
+        "accessKey": process.env.LT_ACCESS_KEY_STAGE,
+        "network": true,
+        "video": true,
+        "console": true,
+        "lambdaLoadExtensions": [
+          // Add private .zip link(s) of the extension uploaded on LambdaTest
+        ],
+        "loadExtensions": [
+          // Add public .zip link(s) of the extension
+        ],
+      },
+    };
+
+    browser = await chromium.connectOverCDP(
+      `wss://cdp.lambdatest.com/playwright-cdp?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    );
+
+    const defaultContext = browser.contexts()[0];
+    const page = defaultContext.pages()[1];
+
+    await page.goto("chrome://extensions/")
+    await page.waitForTimeout(1000)
+
+    let [background] = defaultContext.serviceWorkers();
+    if (!background)
+      background = await defaultContext.waitForEvent("serviceworker");
+    const extensionId = background.url().split("/")[2];
+    console.log("ExtensionId:: ", extensionId);
+
+    await page.goto(`chrome-extension://${extensionId}/hello.html`)
+    await page.waitForTimeout(1000)
+
+    let text = await (await page.$("body > p")).textContent();
+
+    try {
+      expect(text).toEqual('Hello, World!')
+      // Mark the test as completed or failed
+      await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Data matched' } })}`)
+    } catch (e) {
+      await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: e.stack } })}`)
+    }
+
+    await browser.close();
+  } catch (e) {
+    await browser.close();
+  }
+})();

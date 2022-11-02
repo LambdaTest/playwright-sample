c8d5acaac108745c166c9f9e69871e58be077de6
Mukesh Rana <mukeshr@lambdatest.com>
2022-11-02 21:06:49 +0530

    adding playwright-jest sample

---
+++ playwright-jest-js/jest-playwright.config.js
@@ -0,0 +1,68 @@
+const caps_chromium = {
+  'browserName': 'pw-chromium', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+  'browserVersion': 'latest',
+  'LT:Options': {
+    'platform': 'Windows 10',
+    'build': 'Playwright-jest sample Build',
+    'name': 'Playwright-jest test on Chromium',
+    'user': process.env.LT_USERNAME || 'username',
+    'accessKey': process.env.LT_ACCESS_KEY || 'accessKey',
+    'network': true,
+    'video': true,
+    'console': true,
+    'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
+    'tunnelName': '', // Optional
+    'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+  }
+};
+
+const caps_firefox = {
+  'browserName': 'pw-firefox', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+  'browserVersion': 'latest',
+  'LT:Options': {
+    'platform': 'Windows 10',
+    'build': 'Playwright-jest sample Build',
+    'name': 'Playwright-jest test on Firefox',
+    'user': process.env.LT_USERNAME || 'username',
+    'accessKey': process.env.LT_ACCESS_KEY || 'accessKey',
+    'network': true,
+    'video': true,
+    'console': true,
+    'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
+    'tunnelName': '', // Optional
+    'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+  }
+};
+
+const caps_webkit = {
+  'browserName': 'pw-webkit', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+  'browserVersion': 'latest',
+  'LT:Options': {
+    'platform': 'Windows 10',
+    'build': 'Playwright-jest sample Build',
+    'name': 'Playwright-jest test on Webkit',
+    'user': process.env.LT_USERNAME || 'username',
+    'accessKey': process.env.LT_ACCESS_KEY || 'accessKey',
+    'network': true,
+    'video': true,
+    'console': true,
+    'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
+    'tunnelName': '', // Optional
+    'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+  }
+};
+
+module.exports = {
+    connectOptions: {
+        chromium: {
+          wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_chromium))}`
+        },
+        firefox: {
+          wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_firefox))}`
+        },
+        webkit: {
+            wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(caps_webkit))}`
+        }
+      },
+      browsers: ['chromium', 'firefox', 'webkit'],
+}
---
+++ playwright-jest-js/pw-jest.test.js
@@ -0,0 +1,16 @@
+// Needs to be higher than the default Playwright timeout
+jest.setTimeout(40 * 1000)
+
+describe("Bing Search", () => {
+  beforeAll(async () => {
+    await page.goto('https://www.bing.com')
+  })
+  it('title should contain LambdaTest - Bing Search', async () => {
+    await page.type('[aria-label="Enter your search term"]', "LambdaTest Blog");
+    await page.press('[aria-label="Enter your search term"]', "Enter");
+    const title = await page.title()
+    console.log('Page title:: ', title)
+    expect(title).toBe('LambdaTest Blog - Search')
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
+  })
+})

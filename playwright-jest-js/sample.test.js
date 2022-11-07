fa527867885a99f7740a1e0f2b8ad571ae3d4b9b
Mukesh Rana <mukeshr@lambdatest.com>
2022-11-07 16:48:09 +0530

    addressing comments

--- playwright-jest-js/sample.test.js
+++ playwright-jest-js/sample.test.js
@@ -2,15 +2,24 @@
 jest.setTimeout(40 * 1000)
 
 describe("Bing Search", () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await page.goto('https://www.bing.com')
   })
-  it('title should contain LambdaTest - Bing Search', async () => {
+  it('title should contain LambdaTest Blog- Bing Search', async () => {
     await page.type('[aria-label="Enter your search term"]', "LambdaTest Blog");
     await page.press('[aria-label="Enter your search term"]', "Enter");
     const title = await page.title()
     console.log('Page title:: ', title)
     expect(title).toBe('LambdaTest Blog - Search')
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
+  })
+
+  it('title should contain LambdaTest- Bing Search', async () => {
+    await page.type('[aria-label="Enter your search term"]', "LambdaTest");
+    await page.press('[aria-label="Enter your search term"]', "Enter");
+    const title = await page.title()
+    console.log('Page title:: ', title)
+    expect(title).toBe('LambdaTest - Search')
     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
   })
 })

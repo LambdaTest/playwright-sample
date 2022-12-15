b96efe34e481039efdc2a0d2d52cd429a8963414
Ankur Narkhede <ankurn@lambdatest.com>
2022-12-15 12:47:49 +0530

    Added smartUI sample code

---
+++ playwright-smartui.js
@@ -0,0 +1,49 @@
+const { chromium } = require('playwright')
+const { expect } = require('@playwright/test');
+
+(async () => {
+  const capabilities = {
+    'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'Windows 10',
+      'build': 'Playwright Sample Build',
+      'name': 'Playwright Sample Test',
+      'user': process.env.LT_USERNAME,
+      'accessKey': process.env.LT_ACCESS_KEY,
+      'network': true,
+      'video': true,
+      'console': true,
+      'smartUIProjectName': '<projectName>' //Add the required Smart UI Project name
+    }
+  }
+
+  const browser = await chromium.connect({
+    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto('https://www.bing.com')
+
+  // Add the following command in order to take screenshot in SmartUI
+  await page.evaluate((_) => {},
+    `lambdatest_action: ${JSON.stringify({ action: 'smartui.takeScreenshot', arguments: { fullPage: true, screenshotName: '<Your Screenshot Name>' }
+    })}`) // Add a relevant screenshot name here
+
+  const element = await page.$('[aria-label="Enter your search term"]')
+  await element.click()
+  await element.type('LambdaTest')
+  await element.press('Enter')
+  const title = await page.title()
+
+  try {
+    expect(title).toEqual('LambdaTest - Search')
+    // Mark the test as completed or failed
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Title matched' } })}`)
+  } catch {
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Title not matched' } })}`)
+  }
+
+  await browser.close()
+})()

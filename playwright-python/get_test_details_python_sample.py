f7f534587693b0fc3519dbd345b7840cc63591c6
asad_h <asadhussain@lambdatest.com>
2022-12-27 09:58:02 +0530

    Add sample script for get test details hook in python

---
+++ playwright-python/get_test_details_python_sample.py
@@ -0,0 +1,69 @@
+import json
+import os
+import urllib
+import subprocess
+
+from playwright.sync_api import sync_playwright
+
+capabilities = {
+    'browserName': 'Chrome',  # Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    'browserVersion': 'latest',
+    'LT:Options': {
+        'platform': 'Windows 10',
+        'build': 'Playwright Build',
+        'name': 'Playwright Test',
+        'user': os.getenv('LT_USERNAME'),
+        'accessKey': os.getenv('LT_ACCESS_KEY'),
+        'network': True,
+        'video': True,
+        'console': True,
+        'tunnel': False,  # Add tunnel configuration if testing locally hosted webpage
+        'tunnelName': '',  # Optional
+        'geoLocation': '', # country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+    }
+}
+
+
+def run(playwright):
+    playwrightVersion = str(subprocess.getoutput('playwright --version')).strip().split(" ")[1]
+    capabilities['LT:Options']['playwrightClientVersion'] = playwrightVersion
+    lt_cdp_url = 'wss://cdp.lambdatest.com/playwright?capabilities=' + urllib.parse.quote(
+        json.dumps(capabilities))
+    browser = playwright.chromium.connect(lt_cdp_url)
+    page = browser.new_page()
+    try:
+        page.goto("https://www.bing.com/")
+        page.fill("[aria-label='Enter your search term'] > input", 'LambdaTest')
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(1000)
+
+        title = page.title()
+
+        print("Title:: ", title)
+
+        if "LambdaTest" in title:
+            set_test_status(page, "passed", "Title matched")
+        else:
+            set_test_status(page, "failed", "Title did not match")
+
+        # get test details at runtime using lambdatest hook
+        test_details = get_test_details(page=page)
+        print("Test Details response - ", json.loads(test_details))
+    except Exception as err:
+        print("Error:: ", err)
+        set_test_status(page, "failed", str(err))
+
+    browser.close()
+
+def get_test_details(page):
+    return page.evaluate("_ => {}", "lambdatest_action: {\"action\": \"getTestDetails\"}")
+    
+
+
+def set_test_status(page, status, remark):
+    page.evaluate("_ => {}",
+                  "lambdatest_action: {\"action\": \"setTestStatus\", \"arguments\": {\"status\":\"" + status + "\", \"remark\": \"" + remark + "\"}}");
+
+
+with sync_playwright() as playwright:
+    run(playwright)

086ef229d9eeff14d02c618f70220d3eaa6522e3
alisengul-lambdatest <alis@lambdatest.com>
2022-12-29 11:16:37 +0300

    Test case names are added, passed or failed result set is also added

--- playwright-csharp-xunit/PlaywrightTestParallel.cs
+++ playwright-csharp-xunit/PlaywrightTestParallel.cs
@@ -30,24 +30,52 @@ namespace playwright_csharp_xunit
             return "wss://cdp.lambdatest.com/playwright?capabilities=" + Uri.EscapeDataString(capabilitiesJson);
         }
     
-        [Fact]
+        [Fact(DisplayName = "Browse LambdaTest in https://www.bing.com")]
         public static async Task TestWithChrome()
         {
             using var playwright = await Playwright.CreateAsync();
             await using var browser = await playwright.Chromium.ConnectAsync(GetCdpUrl("Chrome"));
             var page = await browser.NewPageAsync();
             await page.GotoAsync("https://www.bing.com");
+            await page.Locator("[aria-label='Enter your search term']").ClickAsync();
+            await page.FillAsync("[id='sb_form_q']", "LambdaTest");
+            await page.Keyboard.PressAsync("Enter");
+            var title = await page.TitleAsync();
+
+            if (title.Contains("LambdaTest"))
+            {
+                // Use the following code to mark the test status.
+                await SetTestStatus("passed", "Title matched", page);
+            }
+            else {
+                await SetTestStatus("failed", "Title not matched", page);
+            }
             await browser.CloseAsync();
         }
     
-        [Fact]
+        [Fact(DisplayName = "Browse LambdaTest in https://www.google.com")]
         public static async Task TestWithMicrosoftEdge()
         {
             using var playwright = await Playwright.CreateAsync();
             await using var browser = await playwright.Chromium.ConnectAsync(GetCdpUrl("MicrosoftEdge"));
             var page = await browser.NewPageAsync();
             await page.GotoAsync("https://www.google.com");
+            await page.FillAsync("[title='Search']", "LambdaTest");
+            await page.Keyboard.PressAsync("Enter");
+            var title = await page.TitleAsync();
+
+            if (title.Contains("LambdaTest"))
+            {
+                // Use the following code to mark the test status.
+                await SetTestStatus("passed", "Title matched", page);
+            }
+            else {
+                await SetTestStatus("failed", "Title not matched", page);
+            }
             await browser.CloseAsync();
+        }
+        private static async Task SetTestStatus(string status, string remark, IPage page) {
+            await page.EvaluateAsync("_ => {}", "lambdatest_action: {\"action\": \"setTestStatus\", \"arguments\": {\"status\":\"" + status + "\", \"remark\": \"" + remark + "\"}}");
         }
     }
 }

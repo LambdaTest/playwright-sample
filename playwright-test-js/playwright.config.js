8830986773b5fcdb41a8b2275a72c8b7b446048d
Ankur Narkhede <84451419+ankur-lt@users.noreply.github.com>
2023-03-21 11:44:06 +0530

    Update playwright.config.js

--- playwright-test-js/playwright.config.js
+++ playwright-test-js/playwright.config.js
@@ -12,19 +12,19 @@ const config = {
     // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
     // Use additional configuration options provided by Playwright if required: https://playwright.dev/docs/api/class-testconfig
     {
-      name: 'chrome:latest:MacOS Catalina@lambdatest',
+      name: 'chrome:latest:MacOS Ventura@lambdatest',
       use: {
         viewport: { width: 1920, height: 1080 }
       }
     },
     {
-      name: 'chrome:latest:Windows 10@lambdatest',
+      name: 'chrome:latest:Windows 11@lambdatest',
       use: {
         viewport: { width: 1280, height: 720 }
       }
     },
     {
-      name: 'MicrosoftEdge:90:Windows 10@lambdatest',
+      name: 'MicrosoftEdge:90:MacOS Ventura@lambdatest',
       use: {
         ...devices['iPhone 12 Pro Max']
       }

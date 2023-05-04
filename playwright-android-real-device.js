ce94d8cacd88616739cd62d781179c3fba24b984
Ankur Narkhede <84451419+ankur-lt@users.noreply.github.com>
2023-05-04 19:24:16 +0530

    Create playwright-android-real-device.js

---
+++ playwright-android-real-device.js
@@ -0,0 +1,53 @@
+const {_android} = require("playwright");
+const expect = require("chai").expect;
+
+(async () => {
+  const capabilities = {
+    "LT:Options": {
+      "platformName": "android",
+      "deviceName": "Galaxy A12",
+      "platformVersion": "10",
+      "isRealMobile": true,
+      "build": "Playwright android build",
+      "name": "Playwright android test",
+      "user": process.env.LT_USERNAME,
+      "accessKey": process.env.LT_ACCESS_KEY_STAGE,
+      "network": true,
+      "video": true,
+      "console": true,
+      "projectName": "New UI",
+    },
+  };
+
+  let device = await _android.connect(
+      `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(
+          JSON.stringify(capabilities))}`,
+  );
+
+  console.log(`Model:: ${device.model()}, serial:: ${device.serial()}`);
+
+  await device.shell("am force-stop com.android.chrome");
+
+  let context = await device.launchBrowser();
+  let page = await context.newPage();
+
+  await page.goto("https://duckduckgo.com");
+  let element = await page.$("[name=\"q\"]");
+  await element.click();
+  await element.type("Playwright");
+  await element.press("Enter");
+  let title = await page.title();
+
+  try {
+    expect(title).to.equal("Playwright at DuckDuckGo");
+    // Mark the test as completed or failed
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: "setTestStatus", arguments: {status: "passed", remark: "Assertions passed" },})}`);
+  } catch (e) {
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: "setTestStatus", arguments: { status: "failed", remark: e.stack }})}`);
+    console.log("Error:: ", e.stack);
+  }
+
+  await page.close();
+  await context.close();
+  await device.close();
+})();

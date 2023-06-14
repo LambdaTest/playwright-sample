2c8a1f4280ea91d969ce92d1766bdd524180906c
VinayKumar VVS <vinayk@LT-MBP-211.local>
2023-06-14 17:33:37 +0530

    WIP | Fixing the CSharp Tests

--- playwright-csharp/PlaywrightTestonIPad.cs
+++ playwright-csharp/PlaywrightTestonIPad.cs
@@ -42,9 +42,9 @@ class PlaywrightTestonIPad
           await page.GotoAsync("https://www.bing.com");
           await page.Locator("[id='sb_form_q']").ClickAsync();
           await page.FillAsync("[id='sb_form_q']", "LambdaTest");
-          await page.waitForTimeout(1000)
+          await page.WaitForTimeoutAsync(1000);
           await page.Keyboard.PressAsync("Enter");
-          await page.waitForSelector("[class=' b_active'])
+          await page.WaitForSelectorAsync("[class=' b_active']");
           var title = await page.TitleAsync();
 
           if (title.Contains("LambdaTest"))

99f426cab9cbe048c45bdce1b26e28455b677887
VinayKumar VVS <vinayk@LT-MBP-211.local>
2023-06-17 22:04:05 +0530

    WIP | Updating the website logic

--- playwright-jest-js/sample.test.js
+++ playwright-jest-js/sample.test.js
@@ -1,36 +1,36 @@
 // Needs to be higher than the default Playwright timeout
 jest.setTimeout(40 * 1000)
 
-describe("Bing Search", () => {
+describe("DuckDuckGo Search", () => {
   beforeEach(async () => {
-    await page.goto('https://www.bing.com/')
-    await page.waitForLoadState('domcontentloaded')
-    await page.waitForTimeout(3000)
+    await page.goto('https://duckduckgo.com')
   })
 
   afterAll(async () => {
     await page.close()
   })
   
-  it('title should contain LambdaTest Blog - Bing Search', async () => {
-    await page.type('[id="sb_form_q"]', "LambdaTest Blog");
-    await page.waitForTimeout(1000)
-    await page.keyboard.press("Enter");
-    await page.waitForSelector('[class=" b_active"]')
-    const title = await page.title()
+  it('title should contain LambdaTest Blog - DuckDuckGo Search', async () => {
+    let element = await page.$("[name=\"q\"]");
+    await element.click();
+    await element.type("LambdaTest Blog");
+    await element.press("Enter");
+    let title = (await page.title()).toString().trim()
+    // let finalTitle = title.trim()
     console.log('Page title:: ', title)
-    expect(title).toBe('LambdaTest Blog - Search')
+    expect(finalTitle).toBe('LambdaTest Blog at DuckDuckGo')
     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
   })
 
-  it('title should contain LambdaTest - Bing Search', async () => {
-    await page.type('[id="sb_form_q"]', "LambdaTest");
-    await page.waitForTimeout(1000)
-    await page.keyboard.press("Enter");
-    await page.waitForSelector('[class=" b_active"]')
-    const title = await page.title()
+  it('title should contain LambdaTest - DuckDuckGo Search', async () => {
+    let element = await page.$("[name=\"q\"]");
+    await element.click();
+    await element.type("LambdaTest");
+    await element.press("Enter");
+    let title = (await page.title()).toString().trim()
+    // const finalTitle = title.trim()
     console.log('Page title:: ', title)
-    expect(title).toBe('LambdaTest - Search')
+    expect(title).toBe('LambdaTest at DuckDuckGo')
     await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({action: 'setTestStatus',arguments: {status: 'passed',remark: 'Test assertion passed'}})}`);
   })
 })

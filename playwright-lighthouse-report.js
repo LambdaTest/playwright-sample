00167455408fdf4423f716d0f1880a89aa81839a
Ankur Narkhede <ankurn@lambdatest.com>
2023-06-27 18:13:59 +0530

    Add playwright JS test for generating lighthouse reports

---
+++ playwright-lighthouse-report.js
@@ -0,0 +1,53 @@
+const { chromium } = require('playwright')
+const { expect } = require('@playwright/test');
+
+(async () => {
+  const capabilities = {
+    'browserName': 'pw-chromium', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'Windows 10',
+      'build': 'Playwright Single Build',
+      'name': 'Playwright Sample Test',
+      'user': process.env.LT_USERNAME,
+      'accessKey': process.env.LT_ACCESS_KEY_STAGE,
+      'network': true,
+      'video': true,
+      'console': true,
+      'tunnel': false, // Add tunnel configuration if testing locally hosted webpage
+      'tunnelName': '', // Optional
+      'geoLocation': '', // country code can be fetched from https://www.lambdatest.com/capabilities-generator/
+    }
+  }
+
+  const browser = await chromium.connect({
+    wsEndpoint: `wss://stage-cdp.lambdatestinternal.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+  })
+
+  const page = await browser.newPage()
+
+  await page.goto('https://www.bing.com')
+
+  // Generate the lighthouse report for the provided URL
+  await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'lighthouseReport', arguments: { url: 'https://www.bing.com' }})}`)
+
+  const element = await page.$('[id="sb_form_q"]')
+  await element.click()
+  await element.type('LambdaTest')
+  await page.waitForTimeout(1000)
+  await page.keyboard.press('Enter')
+  await page.waitForSelector('[class=" b_active"]')
+  const title = await page.title()
+
+  await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'lighthouseReport', arguments: { url: "https://login.live.com" }})}`)
+
+  try {
+    expect(title).toEqual('LambdaTest - Search')
+    // Mark the test as completed or failed
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'passed', remark: 'Title matched' } })}`)
+  } catch {
+    await page.evaluate(_ => {}, `lambdatest_action: ${JSON.stringify({ action: 'setTestStatus', arguments: { status: 'failed', remark: 'Title not matched' } })}`)
+  }
+
+  await browser.close()
+})()

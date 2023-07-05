a52d7cbd8f4aa5764083aa575a8e3d42485d8c3b
Ankur Narkhede <ankurn@lambdatest.com>
2023-07-05 11:44:43 +0530

    Modified the playwright configs matrix

--- playwright-test-js/playwright.config.js
+++ playwright-test-js/playwright.config.js
@@ -11,32 +11,32 @@ const config = {
     // name in the format: browserName:browserVersion:platform@lambdatest
     // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
     // Use additional configuration options provided by Playwright if required: https://playwright.dev/docs/api/class-testconfig
-    // {
-    //   name: 'chrome:latest:MacOS Ventura@lambdatest',
-    //   use: {
-    //     viewport: { width: 1920, height: 1080 }
-    //   }
-    // },
-    // {
-    //   name: 'chrome:latest:Windows 11@lambdatest',
-    //   use: {
-    //     viewport: { width: 1280, height: 720 }
-    //   }
-    // },
-    // {
-    //   name: 'MicrosoftEdge:109:MacOS Ventura@lambdatest',
-    //   use: {
-    //     ...devices['iPhone 12 Pro Max']
-    //   }
-    // },
-    // {
-    //   name: 'pw-firefox:latest:Windows 10@lambdatest',
-    //   use: {
-    //     viewport: { width: 1280, height: 720 }
-    //   }
-    // },
     {
-      name: 'pw-webkit:latest:Windows 10@lambdatest',
+      name: 'chrome:latest:MacOS Ventura@lambdatest',
+      use: {
+        viewport: { width: 1920, height: 1080 }
+      }
+    },
+    {
+      name: 'chrome:latest:Windows 11@lambdatest',
+      use: {
+        viewport: { width: 1280, height: 720 }
+      }
+    },
+    {
+      name: 'MicrosoftEdge:109:MacOS Ventura@lambdatest',
+      use: {
+        ...devices['iPhone 12 Pro Max']
+      }
+    },
+    {
+      name: 'pw-firefox:latest:Windows 10@lambdatest',
+      use: {
+        viewport: { width: 1280, height: 720 }
+      }
+    },
+    {
+      name: 'pw-webkit:latest:MacOS Ventura@lambdatest',
       use: {
         viewport: { width: 1920, height: 1080 }
       }

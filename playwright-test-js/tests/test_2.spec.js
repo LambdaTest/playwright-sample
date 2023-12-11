2605250f281b5d0be686e1a9680d2125a7b9b6a7
Sindhuja-pathuri <146809935+Sindhuja-pathuri@users.noreply.github.com>
2023-12-11 14:55:38 +0530

    test_2.spec.js

--- playwright-test-js/tests/test_2.spec.js
+++ playwright-test-js/tests/test_2.spec.js
@@ -2,16 +2,21 @@ const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
 test.describe('Browse LambdaTest in different search engines', () => {
-  test('Search LambdaTest Blog on DuckDuckGo', async ({ page }) => {
-    await page.goto('https://duckduckgo.com')
-    let element = await page.locator("[name=\"q\"]");
-    await element.click();
-    await element.type("LambdaTest Blog");
-    await element.press("Enter");
-    const title = await page.title()
+  test('test_2', async({page}) => {
+    
+    
+    await page.goto('https://www.lambdatest.com/selenium-playground/')
+    await page.click("//a[text()='Drag & Drop Sliders']") 
+    
+    const slide = await page.locator("//input[@type='range' and @value='15']")
+    let temp = 15
+    while(temp!=95){
+          let expected = await page.locator("//output[@id='rangeSuccess']")
+          await slide.press('ArrowRight')
+          temp++
 
-    console.log('Page title:: ', title)
-    // Use the expect API for assertions provided by playwright
-    expect(title).toEqual(expect.stringContaining('LambdaTest'))
-  })
+   }
+    await page.waitForTimeout(5000)
+
+})
 })

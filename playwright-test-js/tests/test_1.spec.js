f0ef81e02e928165c2ac6f8341275a40de8c69e3
Sindhuja-pathuri <146809935+Sindhuja-pathuri@users.noreply.github.com>
2023-12-12 11:41:29 +0530

    test_1.spec.js

--- playwright-test-js/tests/test_1.spec.js
+++ playwright-test-js/tests/test_1.spec.js
@@ -1,31 +1,28 @@
 const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
-test.describe('Browse LambdaTest in different search engines', () => {
- test('test_1', async({page}) => { 
-    
-    await page.goto('https://www.lambdatest.com/selenium-playground/')
-    await page.click("//a[text()='Simple Form Demo']")
-    //const url = await expect(page).toHaveURL('https://www.lambdatest.com/selenium-playground/simple-form-demo');
-    const URL = page.url()
-    if(URL.includes('simple-form-demo')){
-        console.log('simple-form-demo')
-    }
-    else{
-        console.log('simple-form-demo is not matched')
-    }
-    //console.log(url)
-    const msg = 'Welcome to Sindhuja Pathuri for Lambda Test'
-    await page.locator("//input[@id='user-message']").fill(msg)
-    await page.click("//button[text()='Get Checked Value']")
-    const msgs = await page.locator("//p[@id='message']").innerText(msg)
-    if(msg==msgs){
-        console.log(msg)
-    }
-    else{
-        console.log('Message is not matched')
-    }
-    await page.waitForTimeout(10000)
+test.describe('Browse LambdaTest in different search engines 2', () => {
+test('Scenario1',async({page})=>{
+await page.goto('https://www.lambdatest.com/selenium-playground')
+await page.click("//a[text()='Simple Form Demo']")
+const urllink=page.url()
+if(urllink.includes('simple-form-demo')){
+console.log('URL contains simple form demo')
+}
+else{
+    console.log('URL doesnot have simple form demo')
+}
+const message='Welcome to Sindhuja for LambdaTest'
 
+await page.locator("//input[@id='user-message']").fill(message)
+await page.click("//button[text()='Get Checked Value']")
+const display=await page.locator("//p[@id='message']").innerText(message)
+if(display==message){
+    console.log(message)
+}
+else{
+    console.log('message is not matched')
+}
+await page.waitForTimeout(5000)
 })
 })

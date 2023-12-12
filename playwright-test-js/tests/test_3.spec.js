61c325c55385f9b2d96aa1dce1c15a903de0e85f
Sindhuja-pathuri <146809935+Sindhuja-pathuri@users.noreply.github.com>
2023-12-12 11:42:20 +0530

    test_3.spec.js

--- playwright-test-js/tests/test_3.spec.js
+++ playwright-test-js/tests/test_3.spec.js
@@ -1,25 +1,36 @@
 const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
-test.describe('Browse LambdaTest in different search engines', () => {
-    test('test_3',async({page})=>{
-        await page.goto("https://www.lambdatest.com/selenium-playground/input-form-demo")
-        //await page.click("//button[text()='Submit']")
-        await expect(page.locator("//button[text()='Submit']")).toHaveText('Please fill out this field')
-        await page.locator("//input[@id='name']").fill("Sindhuja")
-        await page.locator("//input[@id='inputEmail4']").fill("sindhuja.pathuri4@gmail.com")
-        await page.locator("//input[@id='inputPassword4']").fill("Sindhu@1234")
-        await page.locator("//input[@id='company']").fill("ABC")
-        await page.locator("//input[@name='website']").fill("ABC.com")
-        await page.locator("//select[@name='country']").selectOption('United States')
-        await page.locator("//input[@name='city']").fill("NY")
-        await page.locator("//input[@name='address_line1']").fill("xyz")
-        await page.locator("//input[@name='address_line2']").fill("ghy")
-        await page.locator("//input[@placeholder='State']").fill("New York")
-        await page.locator("//input[@name='zip']").fill("37700")
-        await page.click("//button[text()='Submit']")
-        const message=await expect(await page.locator("//p[@style='display: block;']")).toHaveText('Thanks for contacting us, we will get back to you shortly.')
-        await console.log(message)
-        page.waitForTimeout(5000)
-        })
+test.describe('Browse LambdaTest in different search engines 3', () => {
+  test('Scenario3',async({page})=>{
+    await page.goto("https://www.lambdatest.com/selenium-playground/input-form-demo")
+    const msg='Please fill in the fields.'
+     const a= await page.locator("//button[text()='Submit']").click()
+    
+    await page.locator("//input[@id='name']").fill("Sindhuja")
+    await page.locator("//input[@id='inputEmail4']").fill("sindhuja.pathuri4@gmail.com")
+    await page.locator("//input[@id='inputPassword4']").fill("Sindhuja@1234")
+    
+    await page.locator("//input[@id='company']").fill("ABC")
+    await page.locator("//input[@name='website']").fill("ABC.com")
+    await page.click("//select[@name='country']",'United States')
+    await page.locator("//input[@name='city']").fill("NY")
+    await page.locator("//input[@name='address_line1']").fill("xyz")
+    await page.locator("//input[@name='address_line2']").fill("ghy")
+    await page.locator("//input[@placeholder='State']").fill("New York")
+    await page.locator("//input[@name='zip']").fill("37700")
+    await page.click("//button[text()='Submit']")
+    const sucmes='Thanks for contacting us, we will get back to you shortly.'
+    const messag=await page.locator("//p[@style='display: block;']").innerText(sucmes)
+    
+    if(sucmes==messag){
+        console.log(sucmes)
+    }
+    else{
+        console.log('not success')
+    }
+    //await console.log(message)
+    await page.waitForTimeout(10000)
+    })
+    
 })

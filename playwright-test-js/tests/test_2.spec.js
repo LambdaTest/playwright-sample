23c26962907686055ed8538e00642a9847620f3a
Sindhuja-pathuri <146809935+Sindhuja-pathuri@users.noreply.github.com>
2023-12-12 11:41:54 +0530

    test_2.spec.js

--- playwright-test-js/tests/test_2.spec.js
+++ playwright-test-js/tests/test_2.spec.js
@@ -1,22 +1,20 @@
 const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
-test.describe('Browse LambdaTest in different search engines', () => {
-  test('test_2', async({page}) => {
+test.describe('Browse LambdaTest in different search engines 2', () => {
+  test('Scenario2',async({page})=>{
+    await page.goto("https://www.lambdatest.com/selenium-playground/")
+    await page.click("//a[text()='Drag & Drop Sliders']")
+    const slider=await page.locator("//input[@type='range' and @value='15']")
+    let temp =15
+       while(temp!='95'){
+               let expval =await page.locator("//output[@id='rangeSuccess']")
+                await slider.press('ArrowRight')
+            temp++
+             }
+             console.log(temp)
+        await page.waitForTimeout(5000)
     
+    })
     
-    await page.goto('https://www.lambdatest.com/selenium-playground/')
-    await page.click("//a[text()='Drag & Drop Sliders']") 
-    
-    const slide = await page.locator("//input[@type='range' and @value='15']")
-    let temp = 15
-    while(temp!=95){
-          let expected = await page.locator("//output[@id='rangeSuccess']")
-          await slide.press('ArrowRight')
-          temp++
-
-   }
-    await page.waitForTimeout(5000)
-
-})
 })

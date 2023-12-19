1a30230838953ceb01700fc61f5cbed138d43183
Ankur Narkhede <ankurn@lambdatest.com>
2023-12-19 16:48:08 +0530

    Added android test cases

--- playwright-test-js/tests/multipleBrowserContexts.spec.js
+++ playwright-test-js/tests/multipleBrowserContexts.spec.js
@@ -2,7 +2,7 @@ const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
 test.describe.skip('Browse LambdaTest in different browser contexts', () => {
-  test.only('Search LambdaTest & LambdaTest Blog on DuckDuckGo', async ({ page }, testInfo) => {
+  test('Search LambdaTest & LambdaTest Blog on DuckDuckGo', async ({ page }, testInfo) => {
     await page.goto('https://duckduckgo.com')
     let element = await page.locator("[name=\"q\"]");
     await element.click();

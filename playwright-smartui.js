752ec9830474450f53c75e7bc414c0c585db147b
Saurabh <saurabh@lambdatest.com>
2024-02-22 10:59:51 +0530

    SelectDOM

--- playwright-smartui.js
+++ playwright-smartui.js
@@ -14,7 +14,7 @@ const {expect} = require("expect");
       'network': true,
       'video': true,
       'console': true,
-      'smartUIProjectName': process.env.SMARTUI_PROJECT || 'ignore-1',
+      'smartUIProjectName': process.env.SMARTUI_PROJECT || 'SmartUI-PW',
       'smartUIBaseline': false
     }
   }
